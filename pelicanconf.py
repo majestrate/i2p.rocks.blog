66c8cac62e69400728e6f5637ce5a7d27d55955f
Jeff Becker <ampernand@gmail.com>
2016-03-11 10:42:46 -0500

    new theme and boilerplate

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,3 +30,76 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+# begin custom configuration
+
+# path to plugins directory
+PLUGINS_PATHS = ['pelican-plugins']
+
+# plugins in use
+PLUGINS = [
+]
+
+# theme name
+THEME = 'i2p.rocks-theme'
+
+# keywords for meta tag
+KEYWORDS = [
+    "i2p",
+    "development",
+    "nntpchan",
+    "livechan"
+]
+
+# css files in order of include
+CSS_FILES = []
+
+# css theme in use
+css_theme = 'simplecss'
+css_theme_stylesheets = []
+
+css_local_override = 'local.css'
+
+
+# js script files
+JS_FILES = []
+
+# do we want to use jquery?
+use_jquery = False
+# jquery version to use
+jquery_version = "2.2.1"
+
+# jquery related script files
+# in order of include
+jquery_jsfiles = [
+    "jquery-{}.min.map",
+    "jquery-{}.min.js"
+]
+
+# theme related script files
+theme_jsfiles = [
+    "simplecss.min.js",
+    "zjs.min.js"
+]
+
+
+# first append jquery files as needed if enabled
+if use_jquery:
+    # if we want to use jquery append all the required jquery files
+    for js in jquery_jsfiles:
+        JS_FILES.append(js.format(jquery_version))
+
+
+# append css theme script files
+for js in theme_jsfiles:        
+    JS_FILES.append(js)
+
+# add css theme to css stylesheets
+CSS_FILES.append('{}.min.css'.format(css_theme))
+
+# append extra theme stylesheets to css stylesheets
+for css in css_theme_stylesheets:
+    CSS_FILES.append(css)
+
+# add local site stylesheet override
+CSS_FILES.append(css_local_override)
